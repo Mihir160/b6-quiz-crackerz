ab2423d94a83235d5144f36a6d40864d39ccb969
Mihir160 <mihir.roy266@gmail.com>
2022-10-11 17:41:45 +0600

    question serial added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -5,10 +5,10 @@ import Questions from '../Questions/Questions';
 const Quiz = () => {
     const quizData = useLoaderData()
      const {name, questions} = quizData.data
-     console.log(questions)
+    //  console.log(questions)
 
     return (
-        <div className='grid lg:grid-cols-2 '>
+        <div className='grid grid-cols-1 '>
             <div className='mt-4'>
            
             <h1 className='text-3xl text-center'>Quiz of {name}</h1>
@@ -16,7 +16,7 @@ const Quiz = () => {
             <div>
             
                 {
-                    questions.map(question=>(<Questions key={question.id} question={question}></Questions>))
+                    questions.map((question, index)  =>(<Questions index={index} key={question.id} question={question}></Questions>))
                 }
             </div>
             </div>
