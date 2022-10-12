f8151e8256e01944faa28f01879c062d80e45717
Mihir160 <mihir.roy266@gmail.com>
2022-10-12 16:06:33 +0600

    quiz number start 1

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -36,12 +36,13 @@ const Questions = ({question, index}) => {
             
               <div className='card lg:w-full bg-base-100 shadow-xl p-8'>
               <div className='flex items-center justify-between'>
-              <h2 className="card-title">Quiz {index} : {question.question}</h2>
+              <h2 className="card-title">Quiz {index+1} : {question.question}</h2>
               <div >
               <svg onClick={()=> setrightAnswer(question.correctAnswer)}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 lg:w-6 lg:h-6 h-8">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
+                <ToastContainer limit={1} />
               </div>
 
               </div>
@@ -49,9 +50,10 @@ const Questions = ({question, index}) => {
                {
                     question.options.map(answer =>(
                         <div className='flex items-center'>
-                            <input onClick={ () => answerloader(answer, question.correctAnswer)}  type="radio" name="radio-2" className="radio radio-primary" ></input> 
+                            <input onClick={ () => answerloader(answer, question.correctAnswer)}  type="radio" name="radio-2" className="radio radio-primary" /> 
+                            
                             <p className='p-2'>{answer}</p>
-                            <ToastContainer />
+                            
                            
                         </div>
                     ))
